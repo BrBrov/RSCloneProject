7b2eda9978cfe1b2f98c21ef4e0c5b6d66bbab42
brbrov <epoc@list.ru>
2023-02-08 14:40:00 +0300

    feat: add db music

---
+++ source/index.js
@@ -0,0 +1,17 @@
+const pop = require('./popTracks.json');
+const hip = require('./hipPopTracks.json');
+const electronic = require('./electronicTracks.json');
+const rock = require('./rockTracks.json');
+const dance = require('./danceTracks.json');
+const music = require('./musicTracks.json');
+const house = require('./houseTracks.json');
+
+module.exports = {
+  pop,
+  hip,
+  electronic,
+  rock,
+  dance,
+  music,
+  house
+};
---
+++ source/indexer.js
@@ -0,0 +1,32 @@
+//Onle for add tracks for DB!!!!!!!!!!!!!!
+//Not uncluding this into projet!
+
+const { MongoClient } = require('mongodb');
+const dbm = require('./index');
+
+const url = 'mongodb+srv://brbrov:biomed@rsclone.ackgmtt.mongodb.net/test';
+let count = 0;
+
+async function add() {
+	const client = new MongoClient(url);
+	const db = client.db('rsclone');
+  const col = db.collection('dbMusic');
+	for (const genre in dbm) {
+		for (const lette of dbm[genre]) {
+			let file = {};
+			count++;
+			file.id = count;
+			file.artist = lette.subtitle;
+			file.title = lette.title;
+			file.genre = genre;
+			if (Array.isArray(lette.hub.actions)) {
+				file.file = lette.hub.actions[1].uri;
+			}
+			file.logo = lette.share.image;
+			await col.insertOne(file);
+		}
+	}
+	await client.close();
+}
+
+add();
